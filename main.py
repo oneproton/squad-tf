12e5646b9dfe617c9d43136db53e8f85a3ba081a
hczhu <haichao592>
2016-08-30 20:13:44 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,139 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+import os
+import random
+import sys
+import time
+
+import numpy as np
+from six.moves import xrange
+import tensorflow as tf
+
+from tensorflow.models.rnn.translate import data_utils
+from tensorflow.models.rnn.translate import seq2seq_model
+
+tf.app.flags.DEFINE_float("learning_rate", 0.5, "Learning rate.")
+tf.app.flags.DEFINE_float("learning_rate_decay_factor", 0.99,
+                          "Learning rate decays by this much.")
+tf.app.flags.DEFINE_float("max_gradient_norm", 5.0,
+                          "Clip gradients to this norm.")
+tf.app.flags.DEFINE_integer("batch_size", 20,
+                            "Batch size to use during training.")
+tf.app.flags.DEFINE_integer("size", 128, "Size of each model layer.")
+tf.app.flags.DEFINE_integer("num_layers", 1, "Number of layers in the model.")
+tf.app.flags.DEFINE_integer("en_vocab_size", 50000, "English vocabulary size.")
+tf.app.flags.DEFINE_integer("fr_vocab_size", 3, "French vocabulary size.")
+tf.app.flags.DEFINE_string("data_dir", "./data", "Data directory")
+tf.app.flags.DEFINE_string("train_dir", "./ckpt", "Training directory.")
+tf.app.flags.DEFINE_integer("max_train_data_size", 60,
+                            "Limit on the siz   e of training data (0: no limit).")
+tf.app.flags.DEFINE_integer("steps_per_checkpoint", 200,
+                            "How many training steps to do per checkpoint.")
+tf.app.flags.DEFINE_boolean("decode", False,
+                            "Set to True for interactive decoding.")
+tf.app.flags.DEFINE_boolean("self_test", False,
+                            "Run a self-test if this is set to True.")
+
+FLAGS = tf.app.flags.FLAGS
+
+_buckets = [(100, 200), (200, 300), (300, 400), (400, 500), (500, 600), (600, 700), (700, 800)]
+
+
+def read_data(source_path, target_path, max_size=None):
+    """Read data from source and target files and put into buckets.
+
+    Args:
+      source_path: path to the files with token-ids for the source language.
+      target_path: path to the file with token-ids for the target language;
+        it must be aligned with the source file: n-th line contains the desired
+        output for n-th line from the source_path.
+      max_size: maximum number of lines to read, all other will be ignored;
+        if 0 or None, data files will be read completely (no limit).
+
+    Returns:
+      data_set: a list of length len(_buckets); data_set[n] contains a list of
+        (source, target) pairs read from the provided data files that fit
+        into the n-th bucket, i.e., such that len(source) < _buckets[n][0] and
+        len(target) < _buckets[n][1]; source and target are lists of token-ids.
+    """
+    data_set = [[] for _ in _buckets]
+    with tf.gfile.GFile(source_path, mode="r") as source_file:
+        with tf.gfile.GFile(target_path, mode="r") as target_file:
+            source, target = source_file.readline(), target_file.readline()
+            counter = 0
+            while source and target and (not max_size or counter < max_size):
+                counter += 1
+                if counter % 100000 == 0:
+                    print("  reading data line %d" % counter)
+                    sys.stdout.flush()
+                source_ids = [int(x) for x in source.split()]
+                target_ids = [int(x) for x in target.split()]
+                target_ids.append(data_utils.EOS_ID)
+                for bucket_id, (source_size, target_size) in enumerate(_buckets):
+                    if len(source_ids) < source_size and len(target_ids) < target_size:
+                        data_set[bucket_id].append([source_ids, target_ids])
+                        break
+                source, target = source_file.readline(), target_file.readline()
+    return data_set
+
+
+def create_model(session, forward_only):
+    """Create translation model and initialize or load parameters in session."""
+    model = seq2seq_model.Seq2SeqModel(
+        FLAGS.en_vocab_size, FLAGS.fr_vocab_size, _buckets,
+        FLAGS.size, FLAGS.num_layers, FLAGS.max_gradient_norm, FLAGS.batch_size,
+        FLAGS.learning_rate, FLAGS.learning_rate_decay_factor,
+        forward_only=forward_only)
+    ckpt = tf.train.get_checkpoint_state(FLAGS.train_dir)
+    if ckpt and tf.gfile.Exists(ckpt.model_checkpoint_path):
+        print("Reading model parameters from %s" % ckpt.model_checkpoint_path)
+        model.saver.restore(session, ckpt.model_checkpoint_path)
+    else:
+        print("Created model with fresh parameters.")
+        session.run(tf.initialize_all_variables())
+    return model
+
+
+def train():
+    with tf.Session() as sess:
+        # Create Model
+        print("Creating %d layers of %d units." % (FLAGS.num_layers, FLAGS.size))
+        model = create_model(sess, False)
+
+        # Read data into buckets and compute their sizes.
+        print ("Reading development and training data (limit: %d)."
+               % FLAGS.max_train_data_size)
+        train_set = read_data(source_path="./data/train.source",
+                              target_path="./data/train.target",
+                              max_size=64)
+        dev_set = read_data(source_path="./data/dev.source",
+                            target_path="./data/dev.target",
+                            max_size=FLAGS.max_train_data_size)
+        train_bucket_sizes = [len(train_set[b]) for b in xrange(len(_buckets))]
+        train_total_size = float(sum(train_bucket_sizes))
+
+        # A bucket scale is a list of increasing numbers from 0 to 1 that we'll use
+        # to select a bucket. Length of [scale[i], scale[i+1]] is proportional to
+        # the size if i-th training bucket, as used later.
+        train_buckets_scale = [sum(train_bucket_sizes[:i + 1]) / train_total_size
+                               for i in xrange(len(train_bucket_sizes))]
+
+    step_time, loss = 0.0, 0.0
+    current_step = 0
+    previous_losses = []
+    while True:
+        random_number_01 = np.random.random_sample()
+        bucket_id = min([i for i in xrange(len(train_buckets_scale)) if train_buckets_scale[i] > random_number_01])
+
+        start_time = time.time()
+        encoder_inputs, decoder_inputs, target_weights = model.get_batch(train_set, bucket_id)
+        print(bucket_id,encoder_inputs,decoder_inputs,target_weights)
+
+
+
+if __name__ == "__main__":
+    print(FLAGS.train_dir)
+    train()
