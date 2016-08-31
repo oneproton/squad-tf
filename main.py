326a9c8059364d07cdcbb65ef5fb658e197b38a5
hczhu <haichao750@gamil.com>
2016-08-31 11:11:44 +0800

    add decode()

--- main.py
+++ main.py
@@ -228,6 +228,48 @@ def train():
                 sys.stdout.flush()
 
 
+def decode():
+    with tf.Session() as sess:
+        # Create model and load parameters.
+        model = create_model(sess, True)
+        model.batch_size = 1  # We decode one sentence at a time.
+
+        # Load vocabularies.
+        en_vocab_path = os.path.join(FLAGS.data_dir,
+                                     "vocab%d.en" % FLAGS.en_vocab_size)
+        de_vocab_path = os.path.join(FLAGS.data_dir,
+                                     "vocab%d.de" % FLAGS.de_vocab_size)
+        en_vocab, _ = data_utils.initialize_vocabulary(en_vocab_path)
+        _, rev_fr_vocab = data_utils.initialize_vocabulary(de_vocab_path)
+
+        # Decode from standard input.
+        sys.stdout.write("> ")
+        sys.stdout.flush()
+        sentence = sys.stdin.readline()
+        while sentence:
+            # Get token-ids for the input sentence.
+            token_ids = data_utils.sentence_to_token_ids(tf.compat.as_bytes(sentence), en_vocab)
+            # Which bucket does it belong to?
+            bucket_id = min([b for b in xrange(len(_buckets))
+                             if _buckets[b][0] > len(token_ids)])
+            # Get a 1-element batch to feed the sentence to the model.
+            encoder_inputs, decoder_inputs, target_weights = get_batch(
+                {bucket_id: [(token_ids, [])]}, bucket_id)
+            # Get output logits for the sentence.
+            _, _, output_logits = model.step(sess, encoder_inputs, decoder_inputs,
+                                             target_weights, bucket_id, True)
+            # This is a greedy decoder - outputs are just argmaxes of output_logits.
+            outputs = [int(np.argmax(logit, axis=1)) for logit in output_logits]
+            # If there is an EOS symbol in outputs, cut them at that point.
+            if data_utils.EOS_ID in outputs:
+                outputs = outputs[:outputs.index(data_utils.EOS_ID)]
+            # Print out French sentence corresponding to outputs.
+            print(" ".join([tf.compat.as_str(rev_fr_vocab[output]) for output in outputs]))
+            print("> ", end="")
+            sys.stdout.flush()
+            sentence = sys.stdin.readline()
+
+
 if __name__ == "__main__":
     print(FLAGS.train_dir)
     train()
